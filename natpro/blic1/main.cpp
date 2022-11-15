e71b8a72b19ebe0c130b934f0465826be3ef43c3
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2022-11-15 03:05:52 +0100

    natpro blic1 cleanup

--- natpro/blic1/main.cpp
+++ natpro/blic1/main.cpp
@@ -36,98 +36,88 @@ const ll INF = 1e9;
 const ld EPS = 1e-9;
 const int INT_INF = 1e6;
 
-int range_sum(int a, int b, std::vector<int> &is_workday_prefix_sum) {
+int range_sum(int a, int b, std::vector<int> &workday_prefix_sum) {
     // caculated number of holidays
     if (a == 0) {
-        return is_workday_prefix_sum[b];
+        return workday_prefix_sum[b];
     } else {
-        return is_workday_prefix_sum[b] - is_workday_prefix_sum[a - 1];
+        return workday_prefix_sum[b] - workday_prefix_sum[a - 1];
     }
 }
 
-int solve(int v, int n, std::vector<int> &is_holiday, int free_days, std::vector<int> &is_workday_prefix_sum) {
+int solve(int v, int n, int s, std::vector<int> &workday_prefix_sum) {
     if (v == INT_INF) return INT_INF;
-    int free_days_int1, sum, new_sum;
+    /*
+        v: length of vacation
+        n: number of year days+
+        s: number of free days
+    */
 
+    int sum;
     for (int i = 0; i <= n - (v * 2); i++) {  // intreval 1
-        dbg("Start of intervalA: ", i);
-        dbg("End of intervalA: ", i + v - 1);
 
-        sum = range_sum(i, i + v - 1, is_workday_prefix_sum);
-        if (sum > free_days) {
-            dbg("cont outer");
+        sum = range_sum(i, i + v - 1, workday_prefix_sum);
+        if (sum > s) {  // stop because the sum is alreaday larger than number of free days
             continue;
         }
-        dbg("Sum of int1", sum);
 
         for (int i2 = i + v; i2 <= n - v; i2++) {  // interval 2 moving head
-
-            dbg("Start of intervalB", i2);
-            dbg("End of intervalB", i2 + v - 1);
-
-            new_sum = (range_sum(i2, i2 + v - 1, is_workday_prefix_sum));
-            dbg("Sum of intB: ", new_sum);
-            new_sum = new_sum + sum;
-            dbg("Sum of in1 + intB: ", new_sum);
-            if (new_sum <= free_days) {
-                dbg("ret", v);
+            if (sum + range_sum(i2, i2 + v - 1, workday_prefix_sum) <= s) {
                 return v;
             }
         }
     }
-    dbg("ret", INT_INF);
+
     return INT_INF;
 }
-
-// https://stackoverflow.com/questions/28504835/c-11-binary-search-and-lambda-function-use-case
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int n, m, tmp, s, tmp_s, max_holiday, counter;
+    int n, m, tmp, s;
     cin >> n;
     cin >> m;
 
-    std::vector<int> is_holiday(n);  // [0, 1, 1 ,1, 0, 0, 0, 0, 0, 0]
-    std::vector<int> vacation_map;   // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    std::vector<int> is_workday_prefix_sum(n);
+    std::vector<int> vacation_lengths;  // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    std::vector<int> workday_prefix_sum(n, 1);
+
+    // parse holidays, set to 0 because workday is 1 (default)
     for (int i = 0; i < m; i++) {
         cin >> tmp;
-        is_holiday[tmp - 1] = 1;
+        workday_prefix_sum[tmp - 1] = 0;
     }
+
+    for (int i = 1; i < n; i++) {
+        workday_prefix_sum[i] = workday_prefix_sum[i - 1] + workday_prefix_sum[i];
+    }
+
     cin >> s;
+
     if (m == 0) {
         cout << int(s / 2);
         return 0;
     }
 
+    // create a list of vacation lenghts we want to check
     for (int i = 1; i <= n / 2; i++) {
-        vacation_map.push_back(i);
-    }
-
-    for (int i = 0; i < n; i++) {
-        if (i == 0) {
-            is_workday_prefix_sum[i] = 1 - is_holiday[0];
-        } else {
-            is_workday_prefix_sum[i] = is_workday_prefix_sum[i - 1] + (1 - is_holiday[i]);
-        }
+        vacation_lengths.push_back(i);
     }
 
     auto comp = [&](int v1, int v2) -> bool {
         int sv1, sv2;
-        sv1 = solve(v1, n, is_holiday, s, is_workday_prefix_sum);
-        sv2 = solve(v2, n, is_holiday, s, is_workday_prefix_sum);
+        sv1 = solve(v1, n, s, workday_prefix_sum);
+        sv2 = solve(v2, n, s, workday_prefix_sum);
 
         return sv1 < sv2;
     };
 
     std::vector<int>::iterator low;
-    low = lower_bound(vacation_map.begin(), vacation_map.end(), INT_INF, comp);
+    low = lower_bound(vacation_lengths.begin(), vacation_lengths.end(), INT_INF, comp);
 
-    if (low == vacation_map.begin()) {  // no solution found so Ivan can't take a vacation
+    if (low == vacation_lengths.begin()) {  // no solution found so Ivan can't take a vacation
         cout << 0;
     } else {
-        cout << (vacation_map[low - vacation_map.begin() - 1]);
+        cout << (vacation_lengths[low - vacation_lengths.begin() - 1]);
     }
 }
