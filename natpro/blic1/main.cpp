265544f30d342723a594d4112043de253a6b1f27
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2022-11-15 01:57:58 +0100

    add corret blic1 natpro

--- natpro/blic1/main.cpp
+++ natpro/blic1/main.cpp
@@ -19,7 +19,7 @@ void dbg_out(Head H, Tail... T) {
     dbg_out(T...);
 }
 #ifdef DEBUG
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#define dbg(...) dbg_out(__VA_ARGS__)
 #else
 #define dbg(...)
 #endif
@@ -35,135 +35,125 @@ const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 const ld EPS = 1e-9;
 
-void solve() {
+int range_sum(int a, int b, std::vector<int> &is_workday_prefix_sum) {
+    // caculated number of holidays
+    if (a == 0) {
+        return is_workday_prefix_sum[b];
+    } else {
+        return is_workday_prefix_sum[b] - is_workday_prefix_sum[a - 1];
+    }
 }
 
+int solve(int v, int n, std::vector<int> &is_holiday, int free_days, std::vector<int> &is_workday_prefix_sum) {
+    if (v == -1) return 0;
+    dbg("=== SOLVE");
+    dbg("vacation:", v);
+    dbg("free_days", free_days);
+    int free_days_int1, sum, new_sum;
+    for (int i = 0; i < n - (v * 2); i++) {  // intreval 1
+        dbg("Start of intervalA: ", i);
+        dbg("End of intervalA: ", i + v - 1);
+
+        sum = range_sum(i, i + v - 1, is_workday_prefix_sum);
+        if (sum > free_days) {
+            dbg("cont outer");
+            continue;
+        }
+        dbg("Sum of int1", sum);
+
+        for (int i2 = i + v; i2 <= n - v; i2++) {  // interval 2 moving head
+
+            dbg("Start of intervalB", i2);
+            dbg("End of intervalB", i2 + v - 1);
+
+            new_sum = (range_sum(i2, i2 + v - 1, is_workday_prefix_sum));
+            dbg("Sum of intB: ", new_sum);
+            new_sum = new_sum + sum;
+            dbg("Sum of in1 + intB: ", new_sum);
+            if (new_sum <= free_days) {
+                dbg("ret", v);
+                return v;
+            }
+        }
+    }
+    dbg("ret", -1);
+    return -1;
+}
+
+// https://stackoverflow.com/questions/28504835/c-11-binary-search-and-lambda-function-use-case
+
 int main() {
+    dbg();
+    dbg("=== NEW TASK");
+    dbg();
     // n i m (0 ≤ m ≤ n ≤ 5000),
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     int n, m, tmp, s, tmp_s, max_holiday, counter;
-    // cin >> n;
-    // cin >> m;
-    // int holidays[n], possible[n];
+    cin >> n;
+    cin >> m;
 
-    // TODO HERE
-    s = 2;
-    n = 8;
-    std::vector<int> holidays = {0, 1, 1, 0, 0, 1, 1, 1};
-    std::vector<int> possible(n);
+    std::vector<int> is_holiday(n);  // [0, 1, 1 ,1, 0, 0, 0, 0, 0, 0]
+    std::vector<int> vacation_map;   // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    std::vector<int> is_workday_prefix_sum(n);
+    for (int i = 0; i < m; i++) {
+        cin >> tmp;
+        dbg("tmp", tmp);
+        is_holiday[tmp] = 1;
+    }
+    cin >> s;
 
-    // for (int i = 0; i <= m; i++) {
-    //     cin >> tmp;
-    //     holidays[i] = tmp;
+    if (m == 0) {
+        cout << int(s / 2);
+    }
+
+    // if (s == 0) {
+    //     std::vector<int> sums;
+
+    //     int max = 0, counter = 0;
+    //     for (int i = 0; i < n; i++) {
+    //         if (is_holiday[i] == 0) {
+    //             counter++;
+    //         } else {
+    //             sums.push_back(counter);
+    //             counter = 0;
+    //         }
+    //     }
+    //     sort(sums.begin(), sums.end(), std::greater<int>());
+
+    //     cout << max;
     // }
 
+    for (int i = 1; i <= n / 2; i++) {
+        vacation_map.push_back(i);
+    }
+
+    dbg("is_holidat", is_holiday);
+    dbg("vacation_map", vacation_map);
+
     for (int i = 0; i < n; i++) {
-        dbg("i", i, possible);
-        dbg("holi", holidays);
-        tmp_s = s;
-
-        // decrement until we have no free days left
-        // while (tmp_s < -1) {
-        //     max_holiday = 0;
-        //     if (possible[i] == 0) {
-        //         tmp_s--;
-        //     }
-        //     max_holiday++;
-        // }
-        // possible[i] = max_holiday;
-
-        // h = 2
-        // [0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-        // __________
-        //     ___________________________________________
-
-        // h = 2
-        // [0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-        // __________
-        //     __________2
-        //        _______
-        //           ____
-        //              __________________________________
-
-        // [1, 0, 1 ,1 ,1, 0, 1, 1]
-        //  _______________
-        //     ___________________
-
         if (i == 0) {
-            max_holiday = 0;
-            counter = 0;
-            // decrement until  we shave no free days left
-            dbg("decrementing until we use all days");
-            while (tmp_s > -1 && counter < n) {
-                if (holidays[counter] == 0) {
-                    tmp_s--;
-                }
-                counter++;
-                max_holiday++;
-            }
-
-            possible[i] = max_holiday - 1;
-            dbg(possible);
-            continue;
-        }
-
-        if (i > 0) {
-            max_holiday = possible[i - 1];
-            counter = max_holiday;
-            tmp_s = 0;
-
-            // LEFT SIDE CHANGES
-
-            if (holidays[i - 1] == 1 && holidays[i] == 0) {  // the previous day was holiday and today is work-day
-                max_holiday--;
-            }
-            if (holidays[i - 1] == 1 && holidays[i] == 1) {  // previous day and today is holiday. that means that we have just one day less because we are still spending the same amount of time
-                max_holiday--;
-            }
-
-            if (holidays[i - 1] == 0 && holidays[i] == 1) {
-                dbg("jesi li prosao decko");
-                tmp_s++;
-                max_holiday--;
-            }
-
-            dbg(possible);
-            dbg("counter", counter);
-            dbg("max_holiday", max_holiday);
-
-            dbg("finding free days on right side");
-            while (tmp_s > -1 && counter < n) {
-                if ((counter) < n && holidays[counter] == 0) {
-                    dbg("decreasing because of zero at", counter);
-                    tmp_s--;
-                }
-
-                counter++;
-                max_holiday++;
-
-                dbg("counter", counter);
-                dbg("max_holiday", max_holiday);
-                dbg("tmp_s", tmp_s);
-            }
-        }
-
-        possible[i] = max_holiday;
-        dbg(possible);
-        dbg();
-    }
-
-    dbg(possible);
-    cin >> s;
-    // todo: mozemo li nekako doci do duljine?
-    // idea: napravimo array koji sadrzi max duljinu puta za svaki dan i sortiram ju?
-
-    // zamislimo da je zadana duljina i da moramo provjeriti je li mozemo za tu duljinu?
-    // todo: binary search duljina putovanja umjesto for petlje
-
-    for (int i = n / 2; i > 0; i--) {
-        for (int j = 0; j < n; i++) {
+            is_workday_prefix_sum[i] = 1 - is_holiday[0];
+        } else {
+            is_workday_prefix_sum[i] = is_workday_prefix_sum[i - 1] + (1 - is_holiday[i]);
         }
     }
+    dbg("is_workday_prefix_sum", is_workday_prefix_sum);
+
+    auto comp = [&](int v1, int v2) -> bool {
+        int sv1, sv2;
+        dbg("");
+        dbg("Comparing v1, v2", v1, v2);
+        sv1 = solve(v1, n, is_holiday, s, is_workday_prefix_sum);
+        dbg("solve1", sv1);
+        sv2 = solve(v2, n, is_holiday, s, is_workday_prefix_sum);
+        dbg("solve2", sv2);
+
+        return sv1 > sv2;
+    };
+
+    std::vector<int>::iterator low;
+    low = lower_bound(vacation_map.begin(), vacation_map.end(), -1, comp);
+    cout << (vacation_map[low - vacation_map.begin() - 1]);
 }
