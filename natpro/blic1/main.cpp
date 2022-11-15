828bb106fe7c08a1508c62f2dd67f59ffcd68f75
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2022-11-15 02:39:49 +0100

    add blic1 5/5

--- natpro/blic1/main.cpp
+++ natpro/blic1/main.cpp
@@ -34,6 +34,7 @@ const int MAX_N = 1e5 + 5;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 const ld EPS = 1e-9;
+const int INT_INF = 1e6;
 
 int range_sum(int a, int b, std::vector<int> &is_workday_prefix_sum) {
     // caculated number of holidays
@@ -45,12 +46,10 @@ int range_sum(int a, int b, std::vector<int> &is_workday_prefix_sum) {
 }
 
 int solve(int v, int n, std::vector<int> &is_holiday, int free_days, std::vector<int> &is_workday_prefix_sum) {
-    if (v == -1) return 0;
-    dbg("=== SOLVE");
-    dbg("vacation:", v);
-    dbg("free_days", free_days);
+    if (v == INT_INF) return INT_INF;
     int free_days_int1, sum, new_sum;
-    for (int i = 0; i < n - (v * 2); i++) {  // intreval 1
+
+    for (int i = 0; i <= n - (v * 2); i++) {  // intreval 1
         dbg("Start of intervalA: ", i);
         dbg("End of intervalA: ", i + v - 1);
 
@@ -76,17 +75,13 @@ int solve(int v, int n, std::vector<int> &is_holiday, int free_days, std::vector
             }
         }
     }
-    dbg("ret", -1);
-    return -1;
+    dbg("ret", INT_INF);
+    return INT_INF;
 }
 
 // https://stackoverflow.com/questions/28504835/c-11-binary-search-and-lambda-function-use-case
 
 int main() {
-    dbg();
-    dbg("=== NEW TASK");
-    dbg();
-    // n i m (0 ≤ m ≤ n ≤ 5000),
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
@@ -99,38 +94,17 @@ int main() {
     std::vector<int> is_workday_prefix_sum(n);
     for (int i = 0; i < m; i++) {
         cin >> tmp;
-        dbg("tmp", tmp);
-        is_holiday[tmp] = 1;
+        is_holiday[tmp - 1] = 1;
     }
     cin >> s;
-
     if (m == 0) {
         cout << int(s / 2);
+        return 0;
     }
-
-    // if (s == 0) {
-    //     std::vector<int> sums;
-
-    //     int max = 0, counter = 0;
-    //     for (int i = 0; i < n; i++) {
-    //         if (is_holiday[i] == 0) {
-    //             counter++;
-    //         } else {
-    //             sums.push_back(counter);
-    //             counter = 0;
-    //         }
-    //     }
-    //     sort(sums.begin(), sums.end(), std::greater<int>());
-
-    //     cout << max;
-    // }
 
     for (int i = 1; i <= n / 2; i++) {
         vacation_map.push_back(i);
     }
-
-    dbg("is_holidat", is_holiday);
-    dbg("vacation_map", vacation_map);
 
     for (int i = 0; i < n; i++) {
         if (i == 0) {
@@ -139,21 +113,21 @@ int main() {
             is_workday_prefix_sum[i] = is_workday_prefix_sum[i - 1] + (1 - is_holiday[i]);
         }
     }
-    dbg("is_workday_prefix_sum", is_workday_prefix_sum);
 
     auto comp = [&](int v1, int v2) -> bool {
         int sv1, sv2;
-        dbg("");
-        dbg("Comparing v1, v2", v1, v2);
         sv1 = solve(v1, n, is_holiday, s, is_workday_prefix_sum);
-        dbg("solve1", sv1);
         sv2 = solve(v2, n, is_holiday, s, is_workday_prefix_sum);
-        dbg("solve2", sv2);
 
-        return sv1 > sv2;
+        return sv1 < sv2;
     };
 
     std::vector<int>::iterator low;
-    low = lower_bound(vacation_map.begin(), vacation_map.end(), -1, comp);
-    cout << (vacation_map[low - vacation_map.begin() - 1]);
+    low = lower_bound(vacation_map.begin(), vacation_map.end(), INT_INF, comp);
+
+    if (low == vacation_map.begin()) {  // no solution found so Ivan can't take a vacation
+        cout << 0;
+    } else {
+        cout << (vacation_map[low - vacation_map.begin() - 1]);
+    }
 }
